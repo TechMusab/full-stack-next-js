d5d40757566d2570c66be39590a59c74ed2d126b
Musab <joiya4636@gmail.com>
2025-07-29 18:31:00 -0700

    file upload

---
+++ app/components/fileUpload.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+  upload,
+} from "@imagekit/next";
+import { useState } from "react";
+interface FileUploadProps {
+  onSuccess?: (response: any) => void;
+  onPrgress?: (progress: number) => void;
+  fileType?: "image" | "video";
+}
+const FileUpload = ({ onSuccess, onPrgress, fileType }: FileUploadProps) => {
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const vallidateFile = (file: File) => {
+    if (fileType === "video") {
+      if (!file.type.startsWith("video/")) {
+        throw new Error("Invalid file type. Please upload a video file.");
+      }
+    }
+    if (file.size > 100 * 1024 * 1024) {
+      throw new Error("File size exceeds 100MB limit.");
+    }
+    return true;
+  };
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+const file= e.target.files?.[0];
+if(!file || !vallidateFile(file)) {
+    return;
+}
+setUploading(true);
+setError(null);
+try {
+    const authRes=await fetch("/api/auth/imagekit-auth")
+    const authData = await authRes.json();
+    const res=await upload({
+        file,
+        fileName: file.name,
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+        signature: authData.signature,
+        expire: authData.expire,
+        token: authData.token,
+
+        onProgress:(event)=>{
+            if(event.lengthComputable && onPrgress) {
+                const progress = (event.loaded / event.total) * 100;
+                onPrgress(Math.round(progress));
+            }
+        }
+    })
+    onSuccess(res)
+    
+} catch (error) {
+    console.error("Upload failed:", error);
+}
+finally {
+    setUploading(false);
+
+}
+  }
+
+
+  return (
+    <>
+      <input type="file" 
+      accept={fileType==="video" ? "video/*":"image/*" }
+      onChange={handleFileChange}
+       />
+       {
+        uploading && <p>Uploading...</p>
+       }
+     
+    </>
+  );
+};
+
+export default FileUpload;
